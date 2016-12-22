b4b351319671ba072a909553fd337052c2a2e597
meenacseengineering5 <meenacse1997@gmail.com>
2016-12-22 09:16:08 +0530

    Add files via upload

---
+++ p.c
@@ -0,0 +1,10 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int p,n,m;
+printf("\n enter the number and power:");
+scanf("\n%d %d",&n,&p);
+m=pow(n,p);
+printf("\n the power is:%d",m);
+}
